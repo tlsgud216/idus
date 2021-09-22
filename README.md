d3e5fa58808ec8cb73e836ba47c1116ede04cd02
kimsinhyoung <tlsgud216@gmail.com>
2021-09-22 22:30:58 +0900

    dbInit.sql 파일 추가

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # idus
 idus backend
+
+테이블 생성 쿼리문은 프로젝트 루트의 dbInit.sql 파일입니다.
--- dbInit.sql
+++ dbInit.sql
@@ -1,31 +1,57 @@
-CREATE TABLE `member` (
-	`id` BIGINT(19) NOT NULL AUTO_INCREMENT,
-	`createdAt` DATETIME NULL DEFAULT NULL,
-	`updatedAt` DATETIME NULL DEFAULT NULL,
-	`email` VARCHAR(100) NOT NULL COLLATE 'utf8_bin',
-	`gender` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8_bin',
-	`loginId` VARCHAR(255) NOT NULL COLLATE 'utf8_bin',
-	`name` VARCHAR(20) NOT NULL COLLATE 'utf8_bin',
-	`nickname` VARCHAR(30) NOT NULL COLLATE 'utf8_bin',
-	`password` VARCHAR(255) NOT NULL COLLATE 'utf8_bin',
-	`phone` VARCHAR(20) NOT NULL COLLATE 'utf8_bin',
-	PRIMARY KEY (`id`) USING BTREE,
-	UNIQUE INDEX `UK_3gj1odv84axhg8cjrvgx4xxhj` (`loginId`) USING BTREE
-)
-COLLATE='utf8mb4_bin'
-ENGINE=InnoDB
-;
+-- --------------------------------------------------------
+-- 호스트:                          127.0.0.1
+-- 서버 버전:                        8.0.26 - MySQL Community Server - GPL
+-- 서버 OS:                        Win64
+-- HeidiSQL 버전:                  11.3.0.6295
+-- --------------------------------------------------------
 
-CREATE TABLE `orders` (
-	`id` BIGINT(19) NOT NULL AUTO_INCREMENT,
-	`createdAt` DATETIME NULL DEFAULT NULL,
-	`updatedAt` DATETIME NULL DEFAULT NULL,
-	`name` VARCHAR(100) NOT NULL COLLATE 'utf8mb4_bin',
-	`member_id` BIGINT(19) NULL DEFAULT NULL,
-	PRIMARY KEY (`id`) USING BTREE,
-	INDEX `FKleg7wy1ic99waq6gm71sf8tdc` (`member_id`) USING BTREE,
-	CONSTRAINT `FKleg7wy1ic99waq6gm71sf8tdc` FOREIGN KEY (`member_id`) REFERENCES `idus`.`member` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION
-)
-COLLATE='utf8mb4_bin'
-ENGINE=InnoDB
-;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- idus 데이터베이스 구조 내보내기
+CREATE DATABASE IF NOT EXISTS `idus` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `idus`;
+
+-- 테이블 idus.member 구조 내보내기
+CREATE TABLE IF NOT EXISTS `member` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  `email` varchar(100) COLLATE utf8mb4_bin NOT NULL,
+  `gender` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
+  `loginId` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `name` varchar(20) COLLATE utf8mb4_bin NOT NULL,
+  `nickname` varchar(30) COLLATE utf8mb4_bin NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `phone` varchar(20) COLLATE utf8mb4_bin NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_3gj1odv84axhg8cjrvgx4xxhj` (`loginId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- 내보낼 데이터가 선택되어 있지 않습니다.
+
+-- 테이블 idus.orders 구조 내보내기
+CREATE TABLE IF NOT EXISTS `orders` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  `name` varchar(100) COLLATE utf8mb4_bin NOT NULL,
+  `orderCode` varchar(20) COLLATE utf8mb4_bin DEFAULT NULL,
+  `member_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_g5ol98ergh3mpp7t0c7cd580u` (`orderCode`),
+  KEY `FKleg7wy1ic99waq6gm71sf8tdc` (`member_id`),
+  CONSTRAINT `FKleg7wy1ic99waq6gm71sf8tdc` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- 내보낼 데이터가 선택되어 있지 않습니다.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
